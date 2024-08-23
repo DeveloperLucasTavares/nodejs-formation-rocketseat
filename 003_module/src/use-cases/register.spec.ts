4f72907f8b47b3967bcfbf730655a505b45a4d0b
Lucas T <tavareslucasfreitas@gmail.com>
2024-08-23 03:59:43 -0300

    created my first unit test

---
+++ 003_module/src/use-cases/register.spec.ts
@@ -0,0 +1,37 @@
+import { describe, it, expect } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
+import { randomUUID } from 'crypto'
+
+describe('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    const registerUseCase = new RegisterUseCase({
+      async findByEmail(email) {
+        return null
+      },
+
+      async create(data) {
+        return {
+          id: randomUUID(),
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    })
+
+    const { user } = await registerUseCase.execute({
+      name: 'Joseph da Silva',
+      email: 'joejoseph@example.com',
+      password: '123456',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+})
